7ec25fe589eb3682d01cbff324e8efd2e0d767c9
anto-pravin <59083077+anto-pravin@users.noreply.github.com>
2020-04-03 23:11:59 +0530

    update

--- MySql/weather_observation_station_4.sql
+++ MySql/weather_observation_station_4.sql
@@ -1,3 +1,12 @@
+/*Let  be the number of 
+CITY entries in STATION, and 
+let  be the number of distinct 
+CITY names in STATION;
+ query the value of  from STATION. 
+ In other words, find the difference between 
+ the total number of CITY entries in the table 
+ and the number of distinct 
+ CITY entries in the table.*/
 SELECT((SELECT COUNT(*)
     FROM STATION)-(SELECT COUNT(DISTINCT CITY)
     FROM STATION))
