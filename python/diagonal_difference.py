f0c44b0c9b6fc620b48c176c5c3d0b722bac4ee3
anto-pravin <59083077+anto-pravin@users.noreply.github.com>
2020-04-04 05:38:51 +0530

    Create diagonal_difference.py

---
+++ python/diagonal_difference.py
@@ -0,0 +1,71 @@
+'''Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+def diagonalDifference(arr):
+    d1 = 0
+    d2 = 0
+    l = len(arr)
+    for i in range(l):
+        for j in range(l):
+            if i==j:
+                d1 += arr[i][j]
+            if i == l-j-1:
+                d2 += arr[i][j]
+    return abs(d1-d2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
